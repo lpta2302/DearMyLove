c0e120cab2e77e8670cee4610bb5d7b22731e1ef
Thien An <lpta2302@gmail.com>
2024-04-29 23:27:22 +0700

    finish

--- assets/js/playMusic.js
+++ assets/js/playMusic.js
@@ -1,5 +1,12 @@
 export default (() => {
   const audioE = document.querySelector("audio");
-  audioE.autoplay = true;
-  audioE.play();
+  const timing = setInterval(() => {
+    if (audioE.paused) {
+      console.log("hi");
+      audioE.play();
+      if (!audioE.paused) {
+        window.clearInterval(timing);
+      }
+    }
+  }, 1000);
 })();
