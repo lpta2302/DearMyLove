f4cc37a07aec43945b11db28fbce069b957f9ae9
Thien An <lpta2302@gmail.com>
2024-04-29 22:20:41 +0700

    finish

--- assets/js/playMusic.js
+++ assets/js/playMusic.js
@@ -1,40 +1,5 @@
 export default (() => {
-  document.addEventListener(
-    "DOMContentLoaded",
-    function () {
-      const audio =
-        document.querySelector("audio");
-      const isAudioPlaying = localStorage.getItem(
-        "isAudioPlaying",
-      );
-
-      if (isAudioPlaying === "true") {
-        audio.play();
-      }
-
-      audio.addEventListener("play", function () {
-        localStorage.setItem(
-          "isAudioPlaying",
-          "true",
-        );
-      });
-
-      audio.addEventListener(
-        "pause",
-        function () {
-          localStorage.setItem(
-            "isAudioPlaying",
-            "false",
-          );
-        },
-      );
-
-      window.addEventListener(
-        "beforeunload",
-        function () {
-          audio.pause();
-        },
-      );
-    },
-  );
+  const audioE = document.querySelector("audio");
+  audioE.autoplay = true;
+  audioE.play();
 })();
